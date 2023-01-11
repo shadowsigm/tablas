0efdb699c7c97a24788ad2775058c386c362cc5c
Alejandro <=>
2023-01-11 19:36:09 -0300

    Sin style

--- src/App.vue
+++ src/App.vue
@@ -18,14 +18,3 @@ export default {
   }
 }
 </script>
-
-<style scope>
-/* #app { */
-#logo {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: left;
-  color: #2c3e50;
-}
-</style>
